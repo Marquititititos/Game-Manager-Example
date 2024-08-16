61d1e9331bfe2410287525c04f20d9e595d08b09
Marquititititos <142502337+Marquititititos@users.noreply.github.com>
2024-08-16 10:38:59 -0300

    EJERCICIO SINGLETONS

---
+++ Assets/GAMEMANAGER.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GAMEMANAGER : MonoBehaviour
+{
+    public static GAMEMANAGER Instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        } else {
+            Instance = this;
+        }
+
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            ChangeScene("Game");
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            ChangeScene("Menu");
+        }
+    }
+
+    public void ChangeScene(string escena)
+    {
+        SceneManager.LoadScene(escena);
+    }
+}
---
+++ Assets/SoundManagerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour
+{
+    public static SoundManagerScript Instance;
+    public AudioSource sourceSFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        sourceSFX.PlayOneShot(clip);
+    }
+}
